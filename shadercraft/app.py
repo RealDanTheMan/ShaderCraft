3027a689fcac39ec1e82866331835e18d5fc64af
RealDanTheMan <dantheman.py@gmail.com>
2024-07-01 01:39:03 +0100

    App will now spawn graphics view on application start

--- shadercraft/app.py
+++ shadercraft/app.py
@@ -1,5 +1,11 @@
+from .appwindow import AppWindow
+
 def Main() -> int:
     print('Starting Shadercraft')
+
+    window = AppWindow()
+    window.run()
+
     print('Exiting application')
 
     return 0
