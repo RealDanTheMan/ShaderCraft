e69d66ac86c6e693af2ca97d9b907a879b10870b
RealDanTheMan <dantheman.py@gmail.com>
2024-12-11 22:28:33 +0000

    Added ability to update the node location within the graph scene view

--- shadercraft/node.py
+++ shadercraft/node.py
@@ -1,16 +1,35 @@
+from PySide6.QtCore import QPointF, Slot
 from .node_widget import NodeWidget
 
 
 class Node(object):
-    def __init__(self):
+    def __init__(self) -> None:
         self.label: str = "Node Label"
         self.name: str = "Node Name"
         self.guid: int = 0
-        self.widget: None
+        self.widget: NodeWidget = None
+        self.posx: float = 0.0
+        self.posy: float = 0.0
 
-    def initWidget(self):
+    def initWidget(self) -> None:
+        """Create widget object representing this node"""
         self.widget = NodeWidget()
         self.widget.setLabelText(self.label)
+        self.widget.positionChanged.connect(self.onWidgetPositionChanged)
 
     def getWidget(self) -> NodeWidget:
+        """Get handle to the widget representing this node"""
         return self.widget
+
+    @Slot(QPointF)
+    def onWidgetPositionChanged(self, value: QPointF) -> None:
+        """Event handler invoked when bound widget changes position"""
+        self.posx = value.x()
+        self.posy = value.y()
+
+    def setPosition(self, x: float, y: float) -> None:
+        """Upadate position of this node, will also update widget position"""
+        self.posx = x
+        self.posy = y
+        if self.widget:
+            self.widget.setPos(QPointF(x, y))
--- shadercraft/node_widget.py
+++ shadercraft/node_widget.py
@@ -1,10 +1,15 @@
 from PySide6.QtGui import QPainter, QColor
 from PySide6.QtWidgets import QGraphicsItem, QStyleOptionGraphicsItem, QWidget
-from PySide6.QtCore import QRectF, Qt
+from PySide6.QtCore import QRectF, Qt, QPointF, Signal, QObject
 
-class NodeWidget(QGraphicsItem):
+
+class NodeWidget(QObject, QGraphicsItem):
+    positionChanged = Signal(QPointF)
+
     def __init__(self) -> None:
-        super().__init__()
+        QObject.__init__(self, None)
+        QGraphicsItem.__init__(self, None)
+
         self.width: float = 128
         self.height: float = 128
         self.setFlag(QGraphicsItem.ItemIsMovable)
@@ -17,28 +22,45 @@ class NodeWidget(QGraphicsItem):
         self.contentForeground = QColor(250, 250, 250)
 
     def boundingRect(self) -> QRectF:
+        """Get bounding area representing the entire node widget"""
         return QRectF(0, 0, self.width, self.height)
-    
+
     def getLabelArea(self) -> QRectF:
+        """Get bounding area representing the label portion of the node widget"""
         return QRectF(0, 0, self.width, self.labelHeight)
-    
+
     def getContentArea(self) -> QRectF:
+        """Get bounding area representing the content portion of the node widget"""
         return QRectF(0, self.labelHeight, self.width, self.height - self.labelHeight)
-    
+
     def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, widget: QWidget | None = ...) -> None:
+        """Draws the entire widget"""
         # Draw label area
         painter.setBrush(self.labelBackground)
         painter.drawRect(self.getLabelArea())
         painter.drawRect
-        
+
         # Draw label text
         text_area = self.getLabelArea().adjusted(10, 0, 0, 0)
         painter.setBrush(self.labelForeground)
         painter.drawText(text_area, Qt.AlignVCenter | Qt.AlignLeft, self.labelText)
-        
+
         # Draw node content area
         painter.setBrush(self.contentBackground)
         painter.drawRect(self.getContentArea())
 
+    def itemChange(self, change, value):
+        """Override for handling internal widget changes"""
+        if change == QGraphicsItem.ItemPositionChange:
+            self.positionChanged.emit(self.scenePos())
+
+        return super().itemChange(change, value)
+
+    def mouseReleaseEvent(self, event):
+        """Override for handling mouse releases performed on this widget"""
+        super().mouseReleaseEvent(event)
+        self.positionChanged.emit(self.scenePos())
+
     def setLabelText(self, text: str) -> None:
+        """Update text drawn in the label area of the widget"""
         self.labelText = text
