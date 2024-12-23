e84396d6a833efd426332131075967ed23d2fc84
RealDanTheMan <dantheman.py@gmail.com>
2024-12-23 12:27:55 +0000

    Cleaned up NodeGraphView implementation

--- shadercraft/nodegraphview.py
+++ shadercraft/nodegraphview.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
-from typing import Optional
-from PySide6.QtGui import QWheelEvent, QMouseEvent, QWheelEvent
+from PySide6.QtGui import QWheelEvent, QMouseEvent
 from PySide6.QtWidgets import QGraphicsView
-from PySide6.QtCore import QPointF, Qt, QPointF
+from PySide6.QtCore import QPointF, Qt
 
 
 class NodeGraphView(QGraphicsView):
+    """
+    Class that represents the actual view of the node graph scene, its
+    responsible for draw ing the actual nodes and other items on the graph.
+    """
+
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
 
@@ -15,7 +19,7 @@ class NodeGraphView(QGraphicsView):
         self.horizontalScrollBar().disconnect(self)
         self.setTransformationAnchor(QGraphicsView.ViewportAnchor.NoAnchor)
 
-        self._scale: float = 1.0 
+        self._scale: float = 1.0
         self._scale_increment: float = 0.1
         self._pan_mode: bool = False
         self._pan_mouse_pos: QPointF = QPointF()
@@ -32,6 +36,7 @@ class NodeGraphView(QGraphicsView):
             self.scale(1.0 - self._scale_increment, 1.0 - self._scale_increment)
 
     def mousePressEvent(self, event: QMouseEvent) -> None:
+        """Event handler invoken when a mouse button is pressed on top of the view"""
         if event.button() == Qt.MouseButton.RightButton:
             print("Entering mouse panning mode")
             self._pan_mode = True
@@ -40,6 +45,7 @@ class NodeGraphView(QGraphicsView):
             super().mousePressEvent(event)
 
     def mouseReleaseEvent(self, event: QMouseEvent) -> None:
+        """Event handler invoked when mouse button is release on top of the view"""
         if event.button() == Qt.MouseButton.RightButton:
             print("Exiting mouse panning mode")
             self._pan_mode = False
@@ -47,10 +53,10 @@ class NodeGraphView(QGraphicsView):
             super().mouseReleaseEvent(event)
 
     def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        """Event handler invoked when mouse moves within the view"""
         if self._pan_mode:
             mouse_delta = event.position() - self._pan_mouse_pos
             self.translate(mouse_delta.x(), mouse_delta.y())
             self._pan_mouse_pos = event.position()
             return
-        else:
-            super().mouseMoveEvent(event)
+        super().mouseMoveEvent(event)
