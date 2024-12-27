829fc8f10e09e3f19d69014911fe0c1f31ab8d80
RealDanTheMan <dantheman.py@gmail.com>
2024-12-27 15:31:35 +0000

    Added palette panel with shader nodes to the app window

--- shadercraft/appwindow.py
+++ shadercraft/appwindow.py
@@ -1,12 +1,13 @@
-from PySide6.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QMainWindow
+from PySide6.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QMainWindow, QVBoxLayout
 from PySide6.QtCore import Qt
 
 from .asserts import assertTrue
 from .node import Node
-from .shadernodes import OutputShaderNode, ShaderNodeBase
+from .shadernodes import OutputShaderNode, ShaderNodeBase, FloatShaderNode, MulShaderNode
 from .windowbase import Ui_MainWindow
 from .nodegraphscene import NodeGraphScene
 from .nodegraphview import NodeGraphView
+from .nodepalette import NodePaletteWidget
 
 
 class AppWindow(QMainWindow):
@@ -22,14 +23,31 @@ class AppWindow(QMainWindow):
         self.graph_view.setDragMode(QGraphicsView.NoDrag)
         self.ui.NodeGraphFrame.layout().addWidget(self.graph_view)
 
+        self._initPalette()
+
         self.graph_scene: NodeGraphScene = NodeGraphScene()
         self.graph_view.setScene(self.graph_scene)
-
         self.graph_view.update()
         asp = self.graph_view.rect().width() / self.graph_view.rect().height()
         self.graph_view.setSceneRect(0, 0, 2000, 2000/asp)
-
+ 
         self.ui.actionGenerate_Shader_Code.triggered.connect(self.onGenerateShaderCode)
+
+    def _initPalette(self) -> None:
+        """
+        Create and initialise node palatte panel
+        """
+        self.palette_widget: NodePaletteWidget = NodePaletteWidget(self)
+        self.ui.PaletteFrame.setLayout(QVBoxLayout())
+        self.ui.PaletteFrame.layout().addWidget(self.palette_widget)
+
+        self.palette_widget.addPaletteItems([
+            FloatShaderNode,
+            MulShaderNode,
+            OutputShaderNode
+        ])
+
+        self.palette_widget.refresh()
 
     def onGenerateShaderCode(self) -> None:
         """
