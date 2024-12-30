b9502647c6cb67a8e7ba92c177d5d9c6c0db4f27
RealDanTheMan <dantheman.py@gmail.com>
2024-12-30 01:11:49 +0000

    Added in-memeory stylesheets for various widgets

---
+++ shadercraft/styles.py
@@ -0,0 +1,25 @@
+
+
+node_widget_style = """
+   #NodeWidget {
+        background-color: orange;
+   }
+
+   #NodeLabelArea {
+        background-color: grey;
+   }
+
+   #NodeLabelText {
+        font-weight: bold;
+        font-size: 14px;
+   }
+
+   #NodeNameText {
+        font-weight: normal;
+        font-size: 10px;
+   }
+
+   #NodeArea {
+        background-color: #303030;
+   }
+"""
