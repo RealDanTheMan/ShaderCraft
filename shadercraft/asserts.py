ef4b90e83d7009beb10f381b5254d068347cdb17
RealDanTheMan <dantheman.py@gmail.com>
2025-01-08 20:49:19 +0000

    Added new assert function for testing value types

--- shadercraft/asserts.py
+++ shadercraft/asserts.py
@@ -6,10 +6,16 @@ def assertRef(ref, msg: str = None) -> None:
         raise AssertionError(msg or "Assert failed! Reference is None")
 
 
+def assertType(val, val_type: type, msg: str = None) -> None:
+    """Raises assert exception if given value is not of given type"""
+    if not isinstance(val, val_type):
+        raise AssertionError(msg or f"Assert failed! Invalid type, expected {val_type} got {type(val)}")
+
+
 def assertTrue(expr, msg: str = None) -> None:
     """Raises assert exception if expression is not True"""
     if not expr:
-        raise AssertionError(msg or "Assert failed! Reference is None")
+        raise AssertionError(msg or "Assert failed! Expression is false")
 
 
 def assertFalse(expr, msg: str = None) -> None:
