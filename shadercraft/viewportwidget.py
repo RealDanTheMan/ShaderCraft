9893fe962153291f36099654a4245b3a38b9a22a
RealDanTheMan <dantheman.py@gmail.com>
2025-01-17 00:20:23 +0000

    Viewport widget can now swap shaders on the fly

--- shadercraft/viewportwidget.py
+++ shadercraft/viewportwidget.py
@@ -1,3 +1,4 @@
+import os
 import logging as Log
 import OpenGL.GL as GL
 from PySide6.QtWidgets import QWidget
@@ -5,7 +6,7 @@ from PySide6.QtOpenGLWidgets import QOpenGLWidget
 from PySide6.QtGui import QOpenGLContext
 from PySide6.QtCore import QTimer
 
-from .asserts import assertRef, assertTrue
+from .asserts import assertRef, assertTrue, assertType
 from .gfx import GFX, GFXRenderable
 
 
@@ -13,6 +14,7 @@ class ViewportWidget(QOpenGLWidget):
     def __init__(self, parent: QWidget = None) -> None:
         super().__init__(parent)
         self.fallback_shader: GL.GLuint = None
+        self.active_shader: GL.GLuint = None
         self.preview_geo: GFXRenderable = None
 
     def initializeGL(self) -> None:
@@ -21,8 +23,9 @@ class ViewportWidget(QOpenGLWidget):
         Log.info(f"OpenGL Version: {GL.glGetString(GL.GL_VERSION).decode()}")
         self.context().makeCurrent(self.context().surface())
         self.fallback_shader = GFX.createFallbackShaderProgram()
+        self.active_shader = self.fallback_shader
         self.preview_geo = GFX.createSphereRenderable(1, 64, 64)
-        GFX.bindRenderableShader(self.preview_geo, self.fallback_shader)
+        GFX.bindRenderableShader(self.preview_geo, self.active_shader)
 
     def paintGL(self) -> None:
         """Redraw GL surface"""
@@ -57,3 +60,32 @@ class ViewportWidget(QOpenGLWidget):
     def requestRedraw(self) -> None:
         """Redraws the OpenGL viewport"""
         self.update()
+
+    def requestShader(self, vs: str, ps: str) -> bool:
+        """
+        Attempts to load and bind new shader for the preview goemetry.
+
+        Parameters:
+            vs (str) : Filepath to vertex shader file.
+            ps (str) : Filepath to pixel shader file.
+
+        Returns:
+            bool : True of shader load and compile succeded, False otherwise.
+        """
+
+        assertType(vs, str)
+        assertType(ps, str)
+        assertTrue(os.path.exists(vs))
+        assertTrue(os.path.exists(ps))
+
+        self.makeCurrent()
+        shader: GL.GLuint = GFX.createShaderFromFiles(vs, ps)
+        if shader == 0:
+            Log.warning("Failed to load or compile shade source files")
+            self.active_shader = self.fallback_shader
+            return False
+
+        self.active_shader = shader
+
+        GFX.bindRenderableShader(self.preview_geo, self.active_shader)
+        return True
