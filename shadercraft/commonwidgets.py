94d005fa72c932f783c3672517c13dd7d4796f9a
RealDanTheMan <dantheman.py@gmail.com>
2025-01-20 23:13:04 +0000

    Added Float3Property widget for editing vector properties

--- shadercraft/commonwidgets.py
+++ shadercraft/commonwidgets.py
@@ -7,10 +7,12 @@ from PySide6.QtWidgets import (
     QLineEdit,
     QDoubleSpinBox,
     QVBoxLayout,
+    QHBoxLayout,
     QFrame
 )
 
 from .asserts import assertRef, assertTrue, assertType
+from .vectors import Vec3F
 
 
 class CommonWidget(QWidget):
@@ -115,3 +117,82 @@ class FloatProperty(CommonWidget):
     def setReadOnly(self, readonly: bool) -> None:
         """Set value indicating if the inner float widget is editable"""
         self.float_widget.setReadOnly(readonly)
+
+
+class Float3Property(CommonWidget):
+    """
+    Widget that exposes three float fields (x,y,z) to the user.
+    """
+
+    value_changed: Signal = Signal(QWidget, Vec3F)
+
+    def __init__(self, label: str, parent: QWidget = None):
+        super().__init__(parent=parent)
+        self.property_label: str = label
+
+        self.setLayout(QVBoxLayout())
+        self.layout().setContentsMargins(2, 2, 2, 2)
+        self.layout().setSpacing(0)
+        self.frame_widget: QFrame = QFrame(parent=self)
+        self.frame_widget.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
+        self.frame_widget.setLayout(QVBoxLayout())
+        self.frame_widget.layout().setContentsMargins(0, 0, 0, 0)
+        self.frame_widget.layout().setSpacing(4)
+        self.layout().addWidget(self.frame_widget)
+
+        self.label_widget: QLabel = QLabel(self.property_label, parent=self.frame_widget)
+        self.frame_widget.layout().addWidget(self.label_widget)
+
+        self.float_frame: QFrame = QFrame(parent=self.frame_widget)
+        self.frame_widget.layout().addWidget(self.float_frame)
+
+        self.float_frame.setLayout(QHBoxLayout())
+
+        self.floatx_widget: QDoubleSpinBox = QDoubleSpinBox(parent=self.float_frame)
+        self.floaty_widget: QDoubleSpinBox = QDoubleSpinBox(parent=self.float_frame)
+        self.floatz_widget: QDoubleSpinBox = QDoubleSpinBox(parent=self.float_frame)
+        self.float_frame.layout().addWidget(self.floatx_widget)
+        self.float_frame.layout().addWidget(self.floaty_widget)
+        self.float_frame.layout().addWidget(self.floatz_widget)
+
+        self.floatx_widget.valueChanged.connect(self.onValueChanged)
+        self.floaty_widget.valueChanged.connect(self.onValueChanged)
+        self.floatz_widget.valueChanged.connect(self.onValueChanged)
+
+    def onValueChanged(self, value: float) -> None:
+        """
+        Event handler invoked when value changes inside any of the three float fields.
+        """
+        assertType(value, float)
+        self.value_changed.emit(self, self.getValue())
+
+    def getValue(self) -> Vec3F:
+        """
+        Get float values in a form of a list.
+        """
+        x: float = self.floatx_widget.value()
+        y: float = self.floaty_widget.value()
+        z: float = self.floatz_widget.value()
+
+        assertType(x, float)
+        assertType(y, float)
+        assertType(z, float)
+
+        return Vec3F(x, y, z)
+
+    def setValue(self, value: Vec3F) -> None:
+        """
+        Set float value.
+        """
+        assertType(value, Vec3F)
+        self.floatx_widget.setValue(value.x)
+        self.floaty_widget.setValue(value.y)
+        self.floatz_widget.setValue(value.z)
+
+    def setReadOnly(self, readonly: bool) -> None:
+        """
+        Set value indicating if the float fields are editable or not.
+        """
+        self.floatx_widget.setReadOnly(readonly)
+        self.floaty_widget.setReadOnly(readonly)
+        self.floatz_widget.setReadOnly(readonly)
