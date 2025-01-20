abb3f39907224a5497b3568628af502573edd53b
RealDanTheMan <dantheman.py@gmail.com>
2025-01-20 23:15:12 +0000

    Added type asserts to vector constructor

--- shadercraft/vectors.py
+++ shadercraft/vectors.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from dataclasses import dataclass
 
+from .asserts import assertType
+
 
 #dataclass
 class Vec2F:
@@ -8,6 +10,8 @@ class Vec2F:
     y: float = 0.0
 
     def __init__(self, xx: float, yy: float) -> None:
+        assertType(xx, float)
+        assertType(yy, float)
         self.x = xx
         self.y = yy
 
@@ -18,6 +22,9 @@ class Vec3F:
     z: float = 0.0
 
     def __init__(self, xx: float, yy: float, zz: float):
+        assertType(xx, float)
+        assertType(yy, float)
+        assertType(zz, float)
         self.x = xx
         self.y = yy
         self.z = zz
@@ -29,6 +36,10 @@ class Vec4F:
     w: float = 0.0
 
     def __init__(self, xx: float, yy: float, zz: float, ww: float):
+        assertType(xx, float)
+        assertType(yy, float)
+        assertType(zz, float)
+        assertType(ww, float)
         self.x = xx
         self.y = yy
         self.z = zz
